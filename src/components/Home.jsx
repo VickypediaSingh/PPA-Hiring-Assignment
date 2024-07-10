29a2ffcd5f1febf034c0e1d9d89c5c6adc6e804a
Bikramjeet Singh <officialbikramjeet@gmail.com>
2024-07-10 11:10:42 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import banner1 from "../assets/banner1.png";
+
+function Home() {
+  return (
+    <div className="bg-neutralSilver">
+      <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen ">
+        {/*  */}
+
+        {/*  */}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
